bed8b6efcdb46fde5c226b1c401493b10407c1d9
rikitwiki <aibekrenadov@gmail.com>
2024-12-06 13:30:38 +0600

    save

--- backend_new/src/users/user.service.js
+++
@@ -1,9 +0,0 @@
-const User = require('./user.model'); 
-
-async function findOneByEmail(email) {
-  return User.findOne({ where: { email } });
-}
-
-module.exports = {
-  findOneByEmail
-};
--- frontend_new/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 8081  // Установка порта, совпадающего с внешним портом в docker-compose.yml
-  }
-});
